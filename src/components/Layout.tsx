095a23047f297226a803db2b976c87a4289cb4d9
nolec <ca10205@naver.com>
2020-06-29 08:59:41 +0900

    수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,40 +1,41 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect, PropsWithChildren, Props } from "react";
 import styled from "styled-components";
 import Head from "next/head";
 import Link from "next/link";
 import Context from "../Context";
-import {GetServerSidePropsContext, NextPage} from "next";
-import {useRouter} from "next/router";
-import {ParsedUrlQuery} from "querystring";
-
+import { GetServerSidePropsContext, NextComponentType } from "next";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
 const Header = styled.header``;
 const Footer = styled.footer``;
 interface IProps {
-    title: any
+  title: String;
+  children: PropsWithChildren<Element[]>;
 }
-const Layout: NextPage<IProps> = ({children,title}) => {
+const Layout: NextComponentType<IProps> = ({ children }, { title }) => {
+  const { keyword, headTitle, handleHeadTitle } = useContext(Context);
+  const router = useRouter();
+  useEffect(() => {
+    handleHeadTitle(title);
+  }, [router.pathname]);
+  return (
+    <>
+      <Header>
+        <Link href="/search/[keyword]" as={`/search/${keyword}`}>
+          <a>search_page</a>
+        </Link>
+      </Header>
+      {children}
+      <Footer></Footer>
+    </>
+  );
+};
 
-    const {keyword, headTitle,handleHeadTitle} = useContext(Context);
-    const router = useRouter();
-    useEffect(() => {
-        handleHeadTitle(title)
-    },[router.pathname])
-    return (
-        <>
-            <Header>
-                <Link href="/search/[keyword]" as={`/search/${keyword}`}>
-                    <a>search_page</a>
-                </Link>
-            </Header>
-            {children}
-            <Footer></Footer>
-        </>
-    )
-}
-
-export const getServerSideProps = (ctx : GetServerSidePropsContext<ParsedUrlQuery>) => {
-    const title = "검색페이지";
-    return {props : {title}}
-}
+export const getServerSideProps = (
+  ctx: GetServerSidePropsContext<ParsedUrlQuery>
+) => {
+  const title = "검색페이지";
+  return { props: { title } };
+};
 export default Layout;
--- src/pages/search/graphql/index.tsx
+++ src/pages/search/graphql/index.tsx
@@ -1,42 +1,66 @@
-import React from "react"
-import {GetServerSidePropsContext} from "next";
-import {ParsedUrlQuery} from "querystring";
-import {initializeApollo} from "../../../lib/apolloClient";
-import gql from 'graphql-tag'
-import {useLazyQuery, useQuery} from "@apollo/react-hooks";
+import React from "react";
+import { GetServerSidePropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
+import { initializeApollo } from "../../../lib/apolloClient";
+import gql from "graphql-tag";
+import { useLazyQuery, useQuery } from "@apollo/react-hooks";
 
+// const ALL_ARTICLE_QUERY = gql` {
+//         articles(includeInvisible :true){
+//             id
+//             title
+//             subtitle
+//             squareThumbnail
+//             rectangleThumbnail
+//         }
+//     }
+// `;
 
 const ALL_ARTICLE_QUERY = gql` {   
-        articles(includeInvisible :true){
-            id
+    getBarcodes([198142,198142]){
+        goodsId
+        options{
+          categoryId
+          optionId
+          barcode{
+            type
             title
-            subtitle
-            squareThumbnail
-            rectangleThumbnail
+            no
+            optionTitle
+          }
         }
     }
+}
 `;
 
-const GraphTest = ({initialApolloState, unstable_revalidate}: any) => {
-    const [loadGreeting, {called, loading, data}]  = useLazyQuery(ALL_ARTICLE_QUERY);
-    if (called && loading) return <p>Loading ...</p>
-    if (!called) {
-        return <button onClick={() => loadGreeting()}>Load greeting</button>
-    }
-    return <div>{data.articles.map((item : any) => item.title)}</div>
-}
+const GraphTest = ({ initialApolloState, unstable_revalidate }: any) => {
+  const [loadGreeting, { called, loading, data }] = useLazyQuery(
+    ALL_ARTICLE_QUERY
+  );
+  if (called && loading) return <p>Loading ...</p>;
+  if (!called) {
+    return <button onClick={() => loadGreeting()}>Load greeting</button>;
+  }
+  return <div>{data.articles.map((item: any) => item.title)}</div>;
+};
 
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext<ParsedUrlQuery>
+) => {
+  console.log(ctx);
+  const apolloClient = initializeApollo();
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext<ParsedUrlQuery>) => {
-    console.log(ctx);
-    const apolloClient = initializeApollo();
+  await apolloClient.query({
+    query: ALL_ARTICLE_QUERY,
+  });
+  console.log(apolloClient.queryManager.dataStore);
 
-    await apolloClient.query({
-        query: ALL_ARTICLE_QUERY
-    })
-    console.log(apolloClient.queryManager.dataStore);
-
-    return {props: {initialApolloState: apolloClient.cache.extract(), unstable_revalidate: 1}}
-}
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+      unstable_revalidate: 1,
+    },
+  };
+};
 
 export default GraphTest;
